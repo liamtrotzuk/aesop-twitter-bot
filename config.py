dd7e30ebe7040fac15f72057c7d4838987a9b293
Liam A. Trotzuk <59979585+liamtrotzuk@users.noreply.github.com>
2020-05-25 18:34:16 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+  config_vars = {
+    'project_id': [ENTER YOUR PROJECT ID HERE],
+    'API_KEY': '[ENTER YOUR API KEY HERE]'
+    'API_KEY_SECRET': '[ENTER YOUR SECRET API KEY HERE]'
+    'ACCESS_TOKEN': '[ENTER YOUR ACCESS TOKEN HERE]'
+    'ACCESS_TOKEN_SECRET': '[ENTER YOUR SECRET ACCESS TOKEN HERE]'
+    'sql_file_path': 'https://github.com/liamtrotzuk/aesop-twitter-bot/blob/master/body.py'
+}
